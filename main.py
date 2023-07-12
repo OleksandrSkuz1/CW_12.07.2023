d83dae17a5ab6b548a571149b42afead887527d1
OleksandrSkuz1 <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-07-12 17:31:50 +0300

    add

---
+++ main.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+
+    with open("products.json", "r", encoding="utf-8") as file:
+        products = json.load(file)
+        for item in products:
+            price = item["price"].replace("NBSP", "").replace("", "")
+            item["price"] = price
+
