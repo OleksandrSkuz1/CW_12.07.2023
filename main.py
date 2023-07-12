219680a07d5820379ccf58960d08dd95c81f5aca
OleksandrSkuz1 <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-07-12 18:27:52 +0300

    add

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import json
 if __name__ == '__main__':
 
     with open("products.json", "r", encoding="utf-8") as file:
-        products = json.load(file)
-        for item in products:
-            price = item["price"].replace("NBSP", "").replace("", "")
-            item["price"] = price
+         products = json.load(file)
+
+    new_list_product = []
+    for item in products:
+        print(item['value'])
+
+        for value in item['value']:
+            if value.lower() == "A++":
+                 new_list_product.append(item)
+
+
+
+
+
 
 
 
